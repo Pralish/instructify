fd5bfb1a3b418263110d7c7a9cab5798b4cada41
Riteshchaudhary710 <chaudharyritesh710@gmail.com>
2023-10-08 08:19:11 +0545

    fix: redirect to login page after destroy

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,8 @@
 class ApplicationController < ActionController::Base
   include TenantResolvable
   include Pagy::Backend
+
+  def after_sign_out_path_for(user)
+    new_user_session_path
+  end
 end
