a6e315751ba78af20769511224a8b902828cb650
Pralish Kayastha <pralishkayastha@gmail.com>
2023-10-09 10:31:58 +0545

    chore(): remove unwanted chanfes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,4 @@
 class ApplicationController < ActionController::Base
   include TenantResolvable
   include Pagy::Backend
-
-  def after_sign_out_path_for(user)
-    new_user_session_path
-  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
-  devise_scope :user do
-    get '/users/sign_out' => 'devise/sessions#destroy'
-  end
 
   resources :roadmaps, only: :show do
     resources :nodes, only: :show
